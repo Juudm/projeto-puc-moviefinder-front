f041ef6a299f2222949664fbdbbbca572f9ff9ef
Juudm <114538749+Juudm@users.noreply.github.com>
2023-06-04 21:20:40 -0300

    Url back api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://localhost:44307/v1/public"
+    baseURL: "https://projetopucmoviefinder.azurewebsites.net/"
 })
